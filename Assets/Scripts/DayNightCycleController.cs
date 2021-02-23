6d42a2ef6070cc78cb621cf5b21b592978140b9a
mfatihdumus <fatihdurmus@live.com>
2021-02-24 00:16:50 +0300

    Day Night cycle with Lightweight Renderer Pipeline

---
+++ Assets/Scripts/DayNightCycleController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[RequireComponent(typeof(Light2D))]
+public class DayNightCycleController : MonoBehaviour
+{
+    public Gradient LightColor;
+
+    [Range(0.0f, 24.0f)]
+    public float Hour;
+
+    private Light2D globalLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        globalLight = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Hour > 20)
+        {
+            Color sunlightColor = LightColor.Evaluate((Hour - 20)/ 4);
+            globalLight.color = sunlightColor;
+        }
+        else if (Hour >= 0 && Hour < 6)
+        {
+            globalLight.color = Color.black;
+        }
+        else
+        {
+            globalLight.color = Color.white;
+        }
+    }
+}
---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeController : MonoBehaviour
+{
+    public DateTime gameDate;
+
+    public float timeInterval = 10;
+
+    private float elapsedTime = 0;
+
+    public Text testLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameDate = new DateTime(1900, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime > timeInterval)
+        {
+            elapsedTime = 0;
+            gameDate = gameDate.AddMinutes(10);
+
+            testLabel.text = gameDate.ToString("MM/dd HH:mm");
+        }
+    }
+}
